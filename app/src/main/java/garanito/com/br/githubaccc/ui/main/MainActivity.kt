d0545bf49330ebf6a58c2915e0699131224e520f
Marcos Garanito <mgaranito@hotmail.com>
2019-02-09 15:25:39 -0200

    Nossa!!!

--- app/src/main/java/garanito/com/br/githubaccc/ui/main/MainActivity.kt
+++ app/src/main/java/garanito/com/br/githubaccc/ui/main/MainActivity.kt
@@ -2,12 +2,38 @@ package garanito.com.br.githubaccc.ui.main
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import dagger.android.AndroidInjection
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.support.HasSupportFragmentInjector
 import garanito.com.br.githubaccc.R
+import garanito.com.br.githubaccc.ui.userprofile.UserProfileFragment
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),HasSupportFragmentInjector {
+
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
+
+    override fun supportFragmentInjector(): AndroidInjector<Fragment> {
+        return dispatchingAndroidInjector
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpDagger()
+        setUpFragment()
+    }
+
+    private fun setUpDagger(){
+        AndroidInjection.inject(this)
+    }
+
+    fun setUpFragment(){
+        supportFragmentManager.beginTransaction()
+                .add(R.id.container,UserProfileFragment(),null)
+                .commit()
     }
 }
--- app/src/main/java/garanito/com/br/githubaccc/ui/userprofile/UserProfileFragment.kt
+++ app/src/main/java/garanito/com/br/githubaccc/ui/userprofile/UserProfileFragment.kt
@@ -1,18 +1,21 @@
 package garanito.com.br.githubaccc.ui.userprofile
 
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.squareup.picasso.Picasso
+import dagger.android.support.AndroidSupportInjection
 
 import garanito.com.br.githubaccc.R
+import kotlinx.android.synthetic.main.fragment_user_profile.*
+import javax.inject.Inject
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -20,10 +23,40 @@ private const val ARG_PARAM2 = "param2"
  */
 class UserProfileFragment : Fragment() {
 
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private lateinit var viewModel: UserProfileViewModel
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_user_profile, container, false)
+
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setUpView()
+    }
+
+    fun setUpView() {
+        btPesquisar.setOnClickListener {
+            viewModel.getUser(etUsuario.text.toString())
+            viewModel.user.observe(this, Observer {
+                tvUsuario.text = it?.name
+                Picasso.get().load(it?.avatarURL).into(ivUsuario)
+            })
+        }
+    }
+
+    fun setUpDagger() {
+        AndroidSupportInjection.inject(this)
+    }
+
+
+    fun setUpViewModel() {
+        viewModel = ViewModelProviders.of(this, viewModelFactory).get(UserProfileViewModel::class.java)
     }
 
 
